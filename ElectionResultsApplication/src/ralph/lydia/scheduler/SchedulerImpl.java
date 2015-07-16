8937442e7e5433975897884a6657a15356980fc5
Lydia.Ralph <lydia.e.ralph@gmail.com>
2015-07-16 11:01:51 +0100

    Skeleton scheduler

---
+++ ElectionResultsApplication/src/ralph/lydia/scheduler/SchedulerImpl.java
@@ -0,0 +1,22 @@
+package ralph.lydia.scheduler;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import static java.util.concurrent.TimeUnit.*;
+
+public class SchedulerImpl {
+
+	public void timeKeeper(){
+//		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+//		executor.scheduleAtFixedRate(helloRunnable, 0, 3, SECONDS);
+//		
+//		FileProcessor fp = new FileProcessorImpl();
+//		int count = 0;
+//		do {
+//			fp.processFile();
+//			count++;
+//		} while (count < 700);
+//		System.out.println("Processed " + count + " files");
+		
+	}
+}
