9daaacfdfbd3aa619299342e81ca8a9d0a358e86
Lydia.Ralph <lydia.e.ralph@gmail.com>
2015-07-16 09:46:32 +0100

    Revert "Removed unnecessary FileValidatorException"
    This reverts commit bc4868345ae155f3b203b73eba87d0028552796d.

---
+++ ElectionResultsApplication/src/ralph/lydia/processor/FileValidatorException.java
@@ -0,0 +1,13 @@
+package ralph.lydia.processor;
+
+public class FileValidatorException extends Exception{
+
+	public FileValidatorException(){
+		super();
+	}
+
+    public FileValidatorException(String message){
+       super(message);
+    }
+	
+}
