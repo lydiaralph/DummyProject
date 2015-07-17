b72d5314ea2dcc0e5a9ad74e45e27b0947dd16d8
Lydia.Ralph <lydia.e.ralph@gmail.com>
2015-07-17 23:26:06 +0100

    Refactored ReadXMLFile. Pulled DefaultHandler into separate class, less confusing. Now returns List<ResultModel> as expected.

--- ElectionResultsApplication/src/ralph/lydia/processor/FileProcessorImpl.java
+++ ElectionResultsApplication/src/ralph/lydia/processor/FileProcessorImpl.java
@@ -3,11 +3,13 @@ package ralph.lydia.processor;
 import java.io.File;
 //import java.nio.file.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 import ralph.lydia.utilities.LoadProperties;
+import ralph.lydia.results.ResultModel;
 
 public class FileProcessorImpl implements FileProcessor {
 
@@ -18,6 +20,8 @@ public class FileProcessorImpl implements FileProcessor {
 	public void processFile() {
 		FileLoader loader = new FileLoaderImpl();
 		FileValidator fileValidator = new FileValidator();
+		ReadXMLFile reader = new ReadXMLFile();
+		List<ResultModel> resultList = new ArrayList<ResultModel>();
 
 		try {
 			xmlFile = null; // Resets without having to create new object
@@ -33,6 +37,16 @@ public class FileProcessorImpl implements FileProcessor {
 		}
 
 		try {
+			resultList = ReadXMLFile.readXmlFileAndParseContents(xmlFile.getAbsolutePath());
+	        for(ResultModel model : resultList) {
+	            model.printAllValues();
+	        }
+		} catch(FileValidatorException e){
+			System.out.println(e.getMessage());
+			outboundFolder = "XML_INVALID";
+		}
+			
+		try{
 			moveToOutboundFolder(xmlFile, outboundFolder);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
--- ElectionResultsApplication/src/ralph/lydia/processor/ReadXMLFile.java
+++ ElectionResultsApplication/src/ralph/lydia/processor/ReadXMLFile.java
@@ -1,23 +1,22 @@
 package ralph.lydia.processor;
 
-import application.PartyCode;
 import ralph.lydia.results.ResultModel;
+import ralph.lydia.results.DefaultHandlerExt;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.io.IOException;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * Modelled on {@link http://www.mkyong.com/java/how-to-read-xml-file-in-java-sax-parser mkyong.com} 
- * @author lydia
+ * Modelled on {@link http://www.mkyong.com/java/how-to-read-xml-file-in-java-sax-parser mkyong.com}
+ *
+ * Also modelled on {@link http://howtodoinjava.com/2012/12/18/how-to-parse-an-xml-using-sax-parser-and-defaulthandler/ howtodoinjava.com}
+ * @author lydia.ralph
  *
  */
 
@@ -26,97 +25,20 @@ public class ReadXMLFile {
 	public static List<ResultModel> readXmlFileAndParseContents(String filePath) 
 			throws FileValidatorException {
 		
-		List<ResultModel> resultsList = new ArrayList<ResultModel>();
-		
 		try {
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
-
-			DefaultHandler handler = new DefaultHandler() {
-				
-				private String elementName;
-				ResultModel result = new ResultModel();
-
-				private void setElementName(String s){
-					this.elementName = s;
-				}
-				
-				private String getElementName(){
-					return this.elementName;
-				}
-				
-				@Override
-				public void startElement(String uri, String localName,String qName, 
-						Attributes attributes) throws SAXException {
-
-					System.out.println("Start Element :" + qName);
-
-					this.setElementName(qName);
-					
-//					if (qName.equalsIgnoreCase("partyCode")) {
-//						partyCode = true;
-//					}
-//
-//					if (qName.equalsIgnoreCase("votes")) {
-//						votes = true;
-//					}
-//
-//					if (qName.equalsIgnoreCase("share")) {
-//						share = true;
-//					}
-				}
-
-				@Override
-				public void endElement(String uri, String localName,
-						String qName) throws SAXException {
-
-					System.out.println("End Element :" + qName);
-				}
-
-				@Override
-				public void characters(char ch[], int start, int length) 
-						throws SAXException{
-
-					/** Need to trim whitespace */
-					String value = new String(ch, start, length).trim();
-					
-					if(value.isEmpty()){
-						// TODO: IMPROVE THIS
-						// Nasty hack. I don't like throwing SAXException for what should be FileValidatorException
-						throw new SAXException("Cannot accept empty value for " + this.getElementName() + " in XML file");
-					}
-					
-					System.out.println("Setting value " + value + " as " + elementName);
-					
-					switch(elementName){
-						case "partyCode":
-							result.setPartyCode(value); break;
-						
-						case "votes":
-							result.setVotes(Integer.parseInt(value)); break;
-						
-						case "share":
-							result.setShare(Float.parseFloat(value)); break;
-					}
-					
-					resultsList.add(this.result);
-					
-				}
-
-			};
+			DefaultHandlerExt handler = new DefaultHandlerExt();
 
 			try{
 				saxParser.parse(filePath, handler);
-			}
-			catch(SAXException|IOException e){
+			} catch(SAXException|IOException e){
 				throw new FileValidatorException(e.getMessage());
-				
 			}
 
+			return handler.getResultsList();
 		} catch(ParserConfigurationException|SAXException|FileValidatorException e) {
 			throw new FileValidatorException(e.getMessage());
 		}
-		return resultsList;
 	}
-	
 }
