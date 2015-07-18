c6f8f74ec01292fa2b215bf962f1f17e55cb8f07
Lydia.Ralph <lydia.e.ralph@gmail.com>
2015-07-18 13:52:48 +0100

    Added sleep to main program

--- ElectionResultsApplication/src/application/ElectionResultsApplication.java
+++ ElectionResultsApplication/src/application/ElectionResultsApplication.java
@@ -4,23 +4,24 @@ import ralph.lydia.processor.FileProcessor;
 import ralph.lydia.processor.FileProcessorImpl;
 import ralph.lydia.results.ResultsTable;
 import ralph.lydia.results.ResultsTableImpl;
+import ralph.lydia.utilities.LoadProperties;
 
 public class ElectionResultsApplication {
 
 	public static void main(String[] args){
 		
 		ResultsTable results = new ResultsTableImpl();
-		
-		System.out.println("Hello world");
+		int sleepInterval = Integer.parseInt(LoadProperties.getKeyValue("SLEEP_INTERVAL"));
 		
 		FileProcessor fp = new FileProcessorImpl();
-		int count = 0;
 		do {
-			// implement Scheduler here?
 			results.refreshDisplay(fp.processFile());
-		} while (count < 700);
-		System.out.println("Processed " + count + " files");
-
-		fp.processFile();
+			try{
+				Thread.sleep(sleepInterval);
+			} 
+			catch(InterruptedException e){
+				System.out.println("Unexpected interruption: " + e.getMessage());
+			}
+		} while (true);
 	}
 }
