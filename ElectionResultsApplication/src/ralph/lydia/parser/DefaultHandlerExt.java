528edf94ae8fb26c2ebe9e3d81c2bf52ec74cd82
Lydia.Ralph <lydia.e.ralph@gmail.com>
2015-07-19 20:38:53 +0100

    Removed unnecessary comments

--- ElectionResultsApplication/src/ralph/lydia/parser/DefaultHandlerExt.java
+++ ElectionResultsApplication/src/ralph/lydia/parser/DefaultHandlerExt.java
@@ -18,10 +18,10 @@ public class DefaultHandlerExt extends DefaultHandler{
 	//This is the list which shall be populated while parsing the XML.
 	private ArrayList<ResultModel> resultsList = new ArrayList<ResultModel>();
 
-	//As we read any XML element we will push that in this stack
+	// Push any XML elements into the stack
 	private Stack<String>  elementStack = new Stack<String> ();
 
-	//As we complete one user block in XML, we will push the User instance in userList
+	// As we complete one ResultModel block in XML, we will push the ResultModel instance into list
 	private Stack<ResultModel> objectStack = new Stack<ResultModel> ();
 
 	@Override
@@ -30,30 +30,17 @@ public class DefaultHandlerExt extends DefaultHandler{
 
 		this.elementStack.push(qName);
 
-		//		System.out.println("Start Element :" + qName);
-		//
-		//		this.setElementName(qName);
-
 		//If this is start of 'result' element then prepare a new ResultModel instance and push it in object stack
 		if ("result".equals(qName)){
-			//New User instance
 			ResultModel result = new ResultModel();
 
-			//Set all required attributes in any XML element here itself
-			//            if(attributes != null && attributes.getLength() == 1)
-			//            {
-			//                user.setId(Integer.parseInt(attributes.getValue(0)));
-			//            }
 			this.objectStack.push(result);
 		}
-
 	}
 
 	@Override
 	public void endElement(String uri, String localName,
 			String qName) throws SAXException {
-
-//		System.out.println("End Element :" + qName);
 
 		//Remove last added  element
 		this.elementStack.pop();
@@ -74,15 +61,11 @@ public class DefaultHandlerExt extends DefaultHandler{
 		/** Need to trim whitespace */
 		String value = new String(ch, start, length).trim();
 
-//		System.out.println("Setting value " + value + " as " + elementName);
-
 		if(value.isEmpty()){
 			// TODO: IMPROVE THIS
 			// Nasty hack. I don't like throwing SAXException for what should be FileValidatorException
 			throw new SAXException("Cannot accept empty value for " + elementName + " in XML file");
 		}
-
-
 
 		if (elementName.equals("partyCode")){
 			ResultModel result = (ResultModel) this.objectStack.peek();
@@ -106,7 +89,6 @@ public class DefaultHandlerExt extends DefaultHandler{
 		return this.elementStack.peek();
 	}
 
-    //Accessor for userList object
     public ArrayList<ResultModel> getResultsList()
     {
         return resultsList;
